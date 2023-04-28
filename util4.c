03fd99745d972b855cedc1d1114305f7cf4664fb
Onome Temiagin <nomsy66@gmail.com>
2023-04-28 16:28:19 -0600

    handle %x

--- util4.c
+++ util4.c
@@ -6,12 +6,9 @@
 #include <string.h>
 #include <unistd.h>
 
-#define CONVERSION_SPECIFIER_D 'd'
-#define CONVERSION_SPECIFIER_I 'i'
 #define CONVERSION_SPECIFIER_U 'u'
 #define CONVERSION_SPECIFIER_O 'o'
 #define CONVERSION_SPECIFIER_X 'x'
-#define CONVERSION_SPECIFIER_X_UPPER 'X'
 
 void handle_ouxX(va_list list_print, int *characters_printed,
 				 char conversion_specifier)
@@ -26,6 +23,6 @@ void handle_ouxX(va_list list_print, int *characters_printed,
 	}
 	else if (conversion_specifier == CONVERSION_SPECIFIER_X)
 	{
-		print_unsigned_integer(list_print, characters_printed, 17, 0);
+		print_unsigned_integer(list_print, characters_printed, 16, 0);
 	}
 }
